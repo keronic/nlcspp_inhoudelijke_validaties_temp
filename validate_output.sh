d45a9558d893327c2c09123de61a7fdae0e96398
Sytse Walraven <sytse.walraven@Mac.keronic.com>
2025-08-07 16:49:18 +0200

    Added annotions to CI run

--- validate_output.sh
+++ validate_output.sh
@@ -2,35 +2,30 @@
 
 version="v11"
 
-for phase_dir in validation_output/$version/*; do
-    phase=$(basename "$phase_dir")
-    for test_dir in $phase_dir/*; do
-        test=$(basename "$test_dir")
-        mkdir -p validation_test_results/$version/$phase/$test
-        java -jar saxon-he.jar -xsl:transformations/validation_output.xsl -s:validation_output/$version/$phase/$test/ -o:validation_test_results/$version/$phase/$test/
-    done
-done
+# for phase_dir in validation_output/$version/*; do
+#     phase=$(basename "$phase_dir")
+#     for test_dir in $phase_dir/*; do
+#         test=$(basename "$test_dir")
+#         mkdir -p validation_test_results/$version/$phase/$test
+#         java -jar saxon-he.jar -xsl:transformations/validation_output.xsl -s:validation_output/$version/$phase/$test/ -o:validation_test_results/$version/$phase/$test/
+#     done
+# done
 
-failed_tests=()
+has_failures=false
 
 # Loop over all files and check contents
 while IFS= read -r -d '' file; do
     if [[ "$(cat "$file")" != "PASS" ]]; then
-        failed_tests+=("$file")
+        # Format output text
+        cleaned_path="${file#validation_test_results/}"
+        cleaned_path="${cleaned_path%.txt}"                         
+        cleaned_path="${cleaned_path//\// | }"
+        
+        echo "::error title=Validation Failed::$cleaned_path"
+        has_failures=true
     fi
 done < <(find validation_test_results/"$version" -type f -print0)
 
-# Report failures if any
-if [[ ${#failed_tests[@]} -gt 0 ]]; then
-    echo "Some validation tests failed:"
-    for f in "${failed_tests[@]}"; do
-        # Format output text
-        cleaned_path="${f#validation_test_results/}"
-        cleaned_path="${cleaned_path%.txt}"                         
-        cleaned_path="${cleaned_path//\//   }"
-        echo "  $cleaned_path"
-    done
+if [[ "$has_failures" == true ]]; then
     exit 1
-else
-    echo "All validation tests passed!"
 fi
