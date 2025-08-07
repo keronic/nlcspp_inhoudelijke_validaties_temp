631d3ce06801135cf21f9c38316f9a24f0572602
Sytse Walraven <sytse.walraven@Mac.keronic.com>
2025-08-07 16:52:38 +0200

    Uncommented generating output

--- validate_output.sh
+++ validate_output.sh
@@ -2,14 +2,14 @@
 
 version="v11"
 
-# for phase_dir in validation_output/$version/*; do
-#     phase=$(basename "$phase_dir")
-#     for test_dir in $phase_dir/*; do
-#         test=$(basename "$test_dir")
-#         mkdir -p validation_test_results/$version/$phase/$test
-#         java -jar saxon-he.jar -xsl:transformations/validation_output.xsl -s:validation_output/$version/$phase/$test/ -o:validation_test_results/$version/$phase/$test/
-#     done
-# done
+for phase_dir in validation_output/$version/*; do
+    phase=$(basename "$phase_dir")
+    for test_dir in $phase_dir/*; do
+        test=$(basename "$test_dir")
+        mkdir -p validation_test_results/$version/$phase/$test
+        java -jar saxon-he.jar -xsl:transformations/validation_output.xsl -s:validation_output/$version/$phase/$test/ -o:validation_test_results/$version/$phase/$test/
+    done
+done
 
 has_failures=false
 
