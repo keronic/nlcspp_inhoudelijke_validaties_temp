bf9a52c06a71d34dc8ac9a8926e3cac4e1bc2024
Sytse Walraven <sytse.walraven@Sytses-MacBook-Pro.local>
2025-08-08 12:42:52 +0200

    Install depencies directly in workflow instead of scripts + only unzip necessary files

--- setup_schxslt2.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-SCHXSLT2_VERSION=1.4.4
-
-curl -L -o schxslt2-${SCHXSLT2_VERSION}.zip https://codeberg.org/SchXslt/schxslt2/releases/download/v${SCHXSLT2_VERSION}/schxslt2-${SCHXSLT2_VERSION}.zip
-unzip -q schxslt2-${SCHXSLT2_VERSION}.zip
-mv schxslt2-${SCHXSLT2_VERSION}/transpile.xsl .
