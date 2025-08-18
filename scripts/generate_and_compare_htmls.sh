919ff2d1fc0889e6e8648b7a5e8596618717c731
Sytse Walraven <sytse.walraven@Sytses-MacBook-Pro.local>
2025-08-18 14:45:42 +0200

    HTML should now pass + updated annotation message

--- scripts/generate_and_compare_htmls.sh
+++ scripts/generate_and_compare_htmls.sh
@@ -3,6 +3,6 @@
 java -jar saxon-he.jar -xsl:doc/NLCSValidatieRegels.xsl -s:doc/NLCSValidatieRegels.xml -o:doc/NLCSValidatieRegels.html
     
 if ! git diff --quiet doc/NLCSValidatieRegels.html; then
-    echo "::error file=doc/NLCSValidatieRegels.html,title=Changes detected in NLCSValidatieRegels.html::Please run java -jar saxon-he.jar -xsl:doc/NLCSValidatieRegels.xsl -s:doc/NLCSValidatieRegels.xml -o:doc/NLCSValidatieRegels.html locally and commit the changed HTML."
+    echo "::error title=Uncommited changes detected in NLCSValidatieRegels.html::NLCSValidatieRegels.html is not in line with NLCSValidatieRegels.xml. Please run the following command locally and commit the changes: java -jar saxon-he.jar -xsl:doc/NLCSValidatieRegels.xsl -s:doc/NLCSValidatieRegels.xml -o:doc/NLCSValidatieRegels.html"
     exit 1
 fi
