4a014fd37c64d1d0cd3b982ebaf81081af2eb4f0
Sytse Walraven <sytse.walraven@Sytses-MacBook-Pro.local>
2025-09-02 11:51:48 +0200

    Deliver through artifacts

---
+++ scripts/bundle.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+version=v11
+out_dir=nlcspp_inhoudelijke_validaties
+
+mkdir -p $out_dir/src/base
+mkdir -p $out_dir/doc
+mkdir -p $out_dir/localization
+
+# Copy base schema
+cp validation_schemas/base_scope_checks/$version.xsl $out_dir/src/base/
+
+# Copy XSL functions
+cp -a validation_schemas/xsl_functions/. $out_dir/src/xsl_functions
+
+# Copy config files
+cp -a configuration/. $out_dir/configuration/
+cp doc/NLCSValidatieRegels.xml $out_dir/doc/
+
+# Copy localization messages
+cp -a localization/* $out_dir/localization/
